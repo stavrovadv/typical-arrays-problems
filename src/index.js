6cb03acf5c242ec24302b7b0131bf655cda9a982
stavrovadv <stavrovadaria97@gmail.com>
2021-03-11 16:22:08 +0500

    feat: add implement min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array !== undefined && array.length !== 0) {
+        let min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array !== undefined && array.length !== 0) {
+        let max = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length !== 0) {
+        let sum = 0;
+        for(let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+    return 0;
 }
